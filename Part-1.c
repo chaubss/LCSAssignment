7da84c52c0c6b1ea6ca43a6fb6e65cea9d701590
= <=>
2020-11-22 23:17:21 +0530

    UPDATED: part1.c

--- Part-1.c
+++ Part-1.c
@@ -1,64 +1,72 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX 100
 
 // MARK:- Stack definition
 
 // A structure to represent a stack
-struct Stack {
+struct Stack
+{
     char top;
     unsigned capacity;
-    char* array;
+    char *array;
 };
- 
+
 // function to create a stack of given capacity. It initializes size of
 // stack as 0
-struct Stack* createStack(unsigned capacity) {
-    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+struct Stack *createStack(unsigned capacity)
+{
+    struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
     stack->capacity = capacity;
     stack->top = '-';
-    stack->array = (char*)malloc(stack->capacity * sizeof(char));
+    stack->array = (char *)malloc(stack->capacity * sizeof(char));
     return stack;
 }
- 
+
 // Stack is full when top is equal to the last index
-int isFull(struct Stack* stack) {
-    return stack->top == stack->capacity - 1;
+int isFull(struct Stack *stack)
+{
+    return (stack->top == (stack->capacity - 1));
 }
- 
+
 // Stack is empty when top is equal to -1
-int isEmpty(struct Stack* stack) {
-    return stack->top == -1;
+int isEmpty(struct Stack *stack)
+{
+    return (stack->top == '-');
 }
- 
+
 // Function to add an item to stack.  It increases top by 1
-void push(struct Stack* stack, char item) {
+void push(struct Stack *stack, char item)
+{
     if (isFull(stack))
         return;
     stack->array[++stack->top] = item;
-    printf("%c pushed to stack\n", item);
 }
- 
+
 // Function to remove an item from stack.  It decreases top by 1
-char pop(struct Stack* stack) {
+char pop(struct Stack *stack)
+{
     if (isEmpty(stack))
         return '-';
     return stack->array[stack->top--];
 }
- 
+
 // Function to return the top from stack without removing it
-char peek(struct Stack* stack) {
+char peek(struct Stack *stack)
+{
     if (isEmpty(stack))
         return '-';
     return stack->array[stack->top];
 }
 
-
 // MARK:- Main Method
 
-int main() {
+int main()
+{
 
     // stack usage ///////////////////////////////
     // struct Stack* stack = createStack(100);
@@ -81,17 +89,72 @@ int main() {
     fgets(tv, MAX, stdin); // has a trailing \n character, ignore it later
 
     printf("%d\n", n);
-    printf("Formula: %s\n", formula);
-    printf("TV: %s\n", tv);
+    printf("Formula:%s\n", formula);
+    printf("TV:%s\n", tv);
 
-    struct Stack* operatorStack = createStack(10);
-    struct Stack* operandStack = createStack(10);
+    struct Stack *operatorStack = createStack(10);
+    struct Stack *operandStack = createStack(10);
 
-    int charSize = sizeof(formula) / sizeof(formula[0]);
-
-    for (int i = 0; i < charSize; i++) {
-        
+    int charSize = strlen(formula) - 1; //ignoring the trailing \n character
+    for (int i = 0; i < charSize; i++)
+    {
+        if (formula[i] == ' ' || formula[i] == '\n')
+        {
+            continue;
+        }
+        char tempChar = formula[i];
+        if (tempChar == '~' || tempChar == '^' || tempChar == 'V' || tempChar == '>' || tempChar == '(')
+        {
+            push(operatorStack, tempChar);
+            printf("Pushed %c into the operator stack\n", tempChar);
+        }
+        else if (tempChar == ')')
+        {
+            char cursor = pop(operatorStack);
+            while (cursor != '(')
+            {
+                if (cursor == '^' || cursor == 'V' || cursor == '>')
+                {
+                    //binary operator
+                    char operand1 = pop(operandStack);
+                    char operand2 = pop(operandStack);
+                    int truthValue1 = (tolower(operand1) == 't') ? 1 : 0;
+                    int truthValue2 = (tolower(operand2) == 't') ? 1 : 0;
+                    int truthValueFinal = 0;
+                    if (cursor == '^')
+                    {
+                        truthValueFinal = truthValue1 & truthValue2;
+                    }
+                    else if (cursor == 'V')
+                    {
+                        truthValueFinal = truthValue1 | truthValue2;
+                    }
+                    else if (cursor == '>')
+                    {
+                        truthValueFinal = !truthValue1 | truthValue2;
+                    }
+                    char operandFinal = (truthValueFinal == 1) ? 'T' : 'F';
+                    push(operandStack, operandFinal);
+                }
+                else
+                {
+                    //unary operator
+                    char operand1 = pop(operatorStack);
+                    int truthValue1 = (tolower(operand1) == 't') ? 1 : 0;
+                    //negation is the only uninary operator
+                    int truthValueFinal = !truthValue1;
+                    char operandFinal = (truthValueFinal == 1) ? 'T' : 'F';
+                    push(operandStack, operandFinal);
+                }
+            }
+        }
+        else
+        {
+            char correspondingTruthValue = tv[tempChar - '0' - 1];
+            push(operandStack, correspondingTruthValue);
+            printf("Pushed %c into the operand stack\n", correspondingTruthValue);
+        }
     }
-
+    printf("%c", pop(operandStack));
     return 0;
 }
